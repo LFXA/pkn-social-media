1b0199de169e43eabf2da303e69db5c5ce000fbe
Lucas Felix Aquino <lucas.aquino@zuhlke.com>
2025-07-13 22:12:01 +0100

    Update pipeline

--- packages/orca-frontend/utils/config.ts
+++ packages/orca-frontend/utils/config.ts
@@ -3,5 +3,5 @@ const API_DEV_URL = 'http://localhost:4000';
 
 export const Config = {
   API_URL: process.env.NODE_ENV === 'production' ? API_PRODUCTION_URL : API_DEV_URL,
-  GOOGLE_ANALYTICS_ID: 'G-MD706N8TZV',
+  GOOGLE_ANALYTICS_ID: '',
 };
